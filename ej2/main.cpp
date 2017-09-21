d2c4dfd7bc1c6f053204c8c2a94fb8bb40906f19
Mike Alche <mikealche@gmail.com>
2017-09-21 10:15:38 -0300

    start developing mst

--- ej2/main.cpp
+++ ej2/main.cpp
@@ -2,6 +2,7 @@
 #include <vector>
 #include <cmath>
 #include <algorithm>
+#include <tuple>
 
 using namespace std;
 
@@ -20,53 +21,84 @@ struct arista {
     }
 };
 
-struct mst {
-    vector<arista>listOfEdges;
-    int totalCost;
-    int root;
 
-};
+
+
 
 class Graph {
+    friend class Mst;
 public:
     Graph(int numberOfNodes, int numberOfEdges);
 
     void addEdge(arista newEdge);
 
-    mst generateMinimumSpanningTree();
+    Mst generateMinimumSpanningTree();
 
 private:
     int nodes;
     int edges;
     vector<arista> listOfEdges;
-    vector<int> parent;
-    vector<int> height;
+    vector<vector<int>> matrixOfEdges;
 
-    int find(int node);
 
-    void uni(int nodeA, int nodeB);
+
+
 };
 
-Graph::Graph(int nodes, int edges) : nodes(nodes),edges(edges) {
-    for (int j = 1; j < nodes +1; ++j) {
-        height.push_back(1);
-        parent.push_back(j);
-    }
-}
+Graph::Graph(int nodes, int edges) : nodes(nodes),edges(edges), matrixOfEdges(nodes, vector<int>(nodes,0)) {}
 
 void Graph::addEdge(arista edge) {
     listOfEdges.push_back(edge);
+    matrixOfEdges[edge.inicio-1][edge.fin-1] = edge.costo;
 }
 
 
-int Graph::find(int node){
+class Mst {
+public:
+    Mst(Graph graph);
+    int getRoot();
+
+private:
+    vector<int> parent;
+    vector<int> height;
+    vector<vector<int>>adjacentEdges;
+    int totalCost;
+    int find(int node);
+    void uni(int nodeA, int nodeB);
+
+    vector<int> getLongestPath();
+
+    tuple<int, int, vector<int>> getFurthestNode(int i);
+
+    int root;
+};
+
+Mst::Mst(Graph graph): adjacentEdges(graph.nodes, vector<int>(0)){
+    for (int j = 1; j < graph.nodes +1; ++j) {
+        height.push_back(1);
+        parent.push_back(j);
+    }
+    totalCost = 0;
+    sort(graph.listOfEdges.begin(), graph.listOfEdges.end()); // ordeno las aristas por peso de menor a mayor
+    for (int i = 0; i < graph.listOfEdges.size(); i++) {
+        arista a = graph.listOfEdges[i];
+        if (find(a.inicio) != find(a.fin)) {
+            adjacentEdges[a.inicio-1].push_back(a.fin);
+            totalCost += a.costo;
+            uni(a.inicio, a.fin);
+        }
+    }
+}
+
+
+int Mst::find(int node){
     if (parent[node -1] != node){
         parent[node-1] = find(parent[node-1]);
     }
     return parent[node -1];
 }
 
-void Graph::uni(int nodeA, int nodeB){
+void Mst::uni(int nodeA, int nodeB){
     nodeA = find(nodeA);
     nodeB = find(nodeB);
     if (height[nodeA-1]<height[nodeB-1]){
@@ -79,28 +111,37 @@ void Graph::uni(int nodeA, int nodeB){
     }
 }
 
-mst Graph::generateMinimumSpanningTree() {
-    //Uses Kruskal algorithm to find MST
-    //Assumes the diagonal of the matrix is 0
-    mst newMst;
-    newMst.totalCost = 0;
-    sort(listOfEdges.begin(), listOfEdges.end()); // ordeno las aristas por peso de menor a mayor
-    for (int i = 0; i < edges; i++) {
-        arista a = listOfEdges[i];
-        if (find(a.inicio) != find(a.fin)) {
-            newMst.listOfEdges.push_back(a);
-            newMst.totalCost += a.costo;
-            uni(a.inicio, a.fin);
-        }
-    }
 
-    return newMst;
+
+int Mst::getRoot(){
+    vector<int> longestPath = getLongestPath();
+
+    root = longestPath[longestPath.size()/2]; //Check if we need +1
+
 
 }
 
+vector<int> Mst::getLongestPath() {
+    //Signature means: from, to, path.
+    tuple<int, int, vector<int>> farawayNode = getFurthestNode(1);
+    tuple<int, int, vector<int>> furthestNode  = getFurthestNode(get<1>(farawayNode));
+
+    return vector<int>();
+}
+
+tuple<int, int, vector<int>> Mst::getFurthestNode(int from) {
+    vector<int> distances(adjacentEdges.size(),0);
 
 
+    //Since there are no cycles, there are no repetitive calls so theres no use of DP
+    //We can do a naive implementation
+    for (int i = 0; i < adjacentEdges[from-1].size(); ++i) {
 
+    }
+
+
+    return tuple<int, int, vector<int>>();
+}
 
 
 int main() {
@@ -120,9 +161,9 @@ int main() {
         cin >> numberOfNodes >> numberOfEdges;
     }
 
-    vector<mst> msts;
+    vector<Mst> msts;
     for (int j = 0; j < graphs.size(); ++j) {
-        mst newMst = graphs[j].generateMinimumSpanningTree();
+        Mst newMst = Mst(graphs[j];
         msts.push_back(newMst);
     }
 
