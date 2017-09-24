64aae2f09624ca88fe93d7abd7cc106db90a0d72
Cat <catalinajuarros@gmail.com>
2017-09-24 19:13:33 -0300

    espero que ahora compile

--- ej1/topdown.cpp
+++ ej1/topdown.cpp
@@ -13,8 +13,8 @@ TallerDeImpresiones::TallerDeImpresiones(int n, MatrizCosto &c) : cantTrabajos(n
     costosAcumulados = MatrizCostoAcumulado(n+1);
     for (int i = 0; i < n+1; ++i) {
         // Inicializa las filas de la matriz. Para cada 0 <= i <= n, crea un vector de i posiciones
-        // que almacenará los costos óptimos cuando t_i es el último trabajo en una máquina y t_j es el
-        // último en la otra, con 0 <= j < i.
+        // que almacenara los costos optimos cuando t_i es el ultimo trabajo en una maquina y t_j es el
+        // ultimo en la otra, con 0 <= j < i.
         for (int j = 0; j < i; ++j) {
             CostoAcumulado costo(0,0);
             costosAcumulados[i].push_back(costo);
@@ -38,13 +38,14 @@ TallerDeImpresiones::TallerDeImpresiones(int n, MatrizCosto &c) : cantTrabajos(n
 
 int TallerDeImpresiones::costoOptimo() {
 
-    /* Metodo principal de la solución top-down.
-       Calcula C(cantTrabajos) mediante llamadas a C moño (cantTrabajos, j)
+    /* Metodo principal de la solucion top-down.
+       Calcula C(cantTrabajos) mediante llamadas a C mono (cantTrabajos, j)
        para cada j entre 0 y cantTrabajos-1.
-IMPORTANTE: modifica la variable global que guarda la solución optima. */
+       IMPORTANTE: modifica la variable global que guarda la solucion optima. */
 
 
     solucionOptima.push(cantTrabajos);
+
     int minimo = costoOptimoAux(cantTrabajos, 0);
     for (int j = 0; j < cantTrabajos; ++j) {
         int actual = costoOptimoAux(cantTrabajos, j);
@@ -60,31 +61,18 @@ stack<int> TallerDeImpresiones::dameSolucionOptima(){
 int TallerDeImpresiones::costoOptimoAux(int i, int j) {
     assert(j >= 0 && i > j);
 
-#ifdef DEBUG
-
-    cout << endl << "i = " << i << ", j = " << j << endl;
-
-#endif
-
     if ( costosAcumulados[i][j].def ) {
+
         // El valor ya estaba guardado en la matriz, lo devolvemos en O(1)
         return costosAcumulados[i][j].valor;
+
     } else {
 
         if (i == 1) {
 
             //Caso base
 
-#ifdef DEBUG
-            cout << endl << "i == 1" << endl;
-#endif
-
             costosAcumulados[1][0] = CostoAcumulado( costosFijos[1][0], 1 );
-
-#ifdef DEBUG
-            mostrarMatriz(costosAcumulados);
-#endif
-
             return costosAcumulados[1][0].valor;
 
         } else {
@@ -93,9 +81,6 @@ int TallerDeImpresiones::costoOptimoAux(int i, int j) {
 
                 // Segundo caso, calculamos la sumatoria
 
-#ifdef DEBUG
-                cout << endl << "j < i-1" << endl;
-#endif
                 int suma = 0;
                 for (int l = j+2; l < i+1; ++l) {
                     suma += costosFijos[l][l-1];
@@ -104,27 +89,16 @@ int TallerDeImpresiones::costoOptimoAux(int i, int j) {
                 int costoParcial = costoOptimoAux(j+1, j); //subproblema
                 costosAcumulados[i][j] = CostoAcumulado( costoParcial + suma, 1 );
 
-#ifdef DEBUG
-                mostrarMatriz(costosAcumulados);
-#endif
-
                 return costosAcumulados[i][j].valor;
 
-            } else { // j == i-1
+            } else {
 
-#ifdef DEBUG
-                cout << endl << "j == i-1" << endl;
-#endif
-
-                /* Elegimos entre las soluciones de i-1 subproblemas. */
+                /* Tercer caso, j = i-1. Elegimos entre las soluciones de i-1 subproblemas. */
 
                 int minimo = costoOptimoAux(j, 0) + costosFijos[i][0], aPushear = 0;
                 for (int k = 1; k < j; ++k ) {
                     int actual = costoOptimoAux(j, k) + costosFijos[i][k];
-#ifdef DEBUG
-                    cout << "costosAcumulados[" << j << "][" << k << "] = " << costosAcumulados[j][k].valor << endl;
-                    cout << "costosFijos[" << i << "][" << k << "] = " << costosFijos[i][k] << endl;
-#endif
+
                     if (actual < minimo) {
                         minimo = actual;
                         aPushear = i;
@@ -132,10 +106,7 @@ int TallerDeImpresiones::costoOptimoAux(int i, int j) {
                     solucionOptima.push(aPushear);
                 }
                 costosAcumulados[i][j] = CostoAcumulado(minimo,1);
-#ifdef DEBUG
-                mostrarMatriz(costosAcumulados);
-                cout << endl;
-#endif
+
                 return minimo;
 
             }
