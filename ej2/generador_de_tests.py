eccb042758d1e03a7a4d8dcf88b3b38deab8cb4d
Cat <catalinajuarros@gmail.com>
2017-09-27 14:54:47 -0300

    Hice una función que genera entradas con grafos telaraña

--- ej2/generador_de_tests.py
+++ ej2/generador_de_tests.py
@@ -15,7 +15,54 @@ def generarPesosUniformes(n, minimo=0, maximo=10):
 def generarPesosNormales(n, mu=5, sigma=1):
     return [ int(max(np.random.normal(mu, sigma), 0)) for i in range(n*(n-1) / 2) ]
 
-def generarGrafo(n, pesos, p_backedge=0.5):
+def sacarBackedges(n, grafo, p_backedge=0.5):
+    # Elimina cada backedge de un grafo con una probabilidad 1.0 - p_backedge.
+    print grafo
+    vecinos = [ [] for i in range(n) ]
+
+    for e in grafo:
+        vecinos[e[0] - 1].append(e[1])
+
+    print vecinos
+
+    return
+
+def generarGrafoSpiderman(n, l, pesos, p_backedge=0.5):
+    # n = número de rayos
+    # l = número de capas
+    assert(len(pesos) == 2*n*l)
+
+    grafoSpiderman = []
+    pesoActual = 0
+
+    # inicializa la primera capa
+    for j in range(2, n+2):
+        grafoSpiderman.append( [1, j, pesos[pesoActual]] )
+
+        if j < n+1:
+            grafoSpiderman.append( [j, j+1, pesos[pesoActual + 1]] )
+        else:
+            grafoSpiderman.append( [2, j, pesos[pesoActual + 1]] )
+
+        pesoActual += 2
+
+    # genera las capas 2 a l
+    for i in range(1, l):
+        for j in range(2, n+2):
+            grafoSpiderman.append( [j + (i-1)*n, j + i*n, pesos[pesoActual]] )
+
+            if j < n+1:
+                grafoSpiderman.append( [j + i*n, j + i*n + 1, pesos[pesoActual + 1]] )
+            else:
+                grafoSpiderman.append( [2 + i*n, j + i*n, pesos[pesoActual + 1]] )
+
+            pesoActual += 2
+
+    grafoStr = [ " ".join( str(k) for k in e ) for e in grafoSpiderman ]
+
+    return grafoStr
+
+def generarGrafoAleatorio(n, pesos, p_backedge=0.5):
     # Es necesario que los pesos se correspondan con la cantidad de aristas
     # de un grafo completo de n nodos
     assert(len(pesos) == n*(n-1) / 2)
@@ -32,15 +79,17 @@ def generarGrafo(n, pesos, p_backedge=0.5):
 
     grafoStr = [ " ".join( str(k) for k in e ) for e in grafoCompleto ]
 
+    sacarBackedges(n, grafoCompleto)
+
     return grafoStr
 
-def generarEntradaPesosUniformes(n, minimo=0, maximo=10, p_backedge=0.5, archivo="output"):
-    # Genera un caso de test con 32 grafos de n nodos. Los pesos están distribuidos
-    # uniformemente según los parámetros especificados.
+def generarEntradaCompletoPesosUniformes(n, minimo=0, maximo=10, p_backedge=0.5, archivo="output"):
+    # Genera un caso de test con 32 grafos de n nodos.
+    # Los pesos están distribuidos uniformemente según los parámetros especificados.
     with open(archivo, 'w') as entrada:
         for i in range(32):
             pesos = generarPesosUniformes(n, minimo, maximo)
-            grafo = generarGrafo(n, pesos)
+            grafo = generarGrafoAleatorio(n, pesos)
             entrada.write(str(n) + " " + str(len(grafo)) + '\n')
             for j in grafo:
                 entrada.write(j + '\n')
@@ -48,13 +97,13 @@ def generarEntradaPesosUniformes(n, minimo=0, maximo=10, p_backedge=0.5, archivo
 
     return
 
-def generarEntradaPesosNormales(n, mu=5, sigma=1, p_backedge=0.5, archivo="output"):
-    # Genera un caso de test con 32 grafos de n nodos. Los pesos están distribuidos
-    # uniformemente según los parámetros especificados.
+def generarEntradaCompletoPesosNormales(n, mu=5, sigma=1, p_backedge=0.5, archivo="output"):
+    # Genera un caso de test con 32 grafos de n nodos.
+    # Los pesos están distribuidos uniformemente según los parámetros especificados.
     with open(archivo, 'w') as entrada:
         for i in range(32):
             pesos = generarPesosNormales(n, mu, sigma)
-            grafo = generarGrafo(n, pesos)
+            grafo = generarGrafoAleatorio(n, pesos)
             entrada.write(str(n) + " " + str(len(grafo)) + '\n')
             for j in grafo:
                 entrada.write(j + '\n')
@@ -62,12 +111,38 @@ def generarEntradaPesosNormales(n, mu=5, sigma=1, p_backedge=0.5, archivo="outpu
 
     return
 
+def generarEntradaSpidermanPesosUniformes(n, l, minimo=0, maximo=0, p_backedge=0.5, archivo="output"):
+    # Genera un caso de test con 32 grafos telaraña de n vértices y l capas.
+    # Los pesos están distribuidos uniformemente según los parámetros especificados.
+    with open(archivo, 'w') as entrada:
+        for i in range(32):
+            pesos = [ np.random.randint(0,10) for i in range(2*n*l) ]
+            grafo = generarGrafoSpiderman(n, l, pesos, p_backedge)
+            entrada.write(str(n*l+1) + " " + str(len(grafo)) + '\n')
+            for j in grafo:
+                entrada.write(j + '\n')
+        entrada.write("0\n")
 
-# pesosUniformes = generarPesosUniformes(10)
-# pesosNormales = generarPesosNormales(10)
+    return
 
-# print generarGrafo(10, pesosUniformes)
-# print generarGrafo(10, pesosNormales)
+def generarEntradaSpidermanPesosNormales(n, l, mu=5, sigma=1, p_backedge=0.5, archivo="output"):
+    # Genera un caso de test con 32 grafos telaraña de n vértices y l capas.
+    # Los pesos están distribuidos uniformemente según los parámetros especificados.
+    with open(archivo, 'w') as entrada:
+        for i in range(32):
+            pesos = [ int(max(np.random.normal(mu, sigma), 0)) for i in range(2*n*l) ]
+            grafo = generarGrafoSpiderman(n, l, pesos, p_backedge)
+            entrada.write(str(n*l+1) + " " + str(len(grafo)) + '\n')
+            for j in grafo:
+                entrada.write(j + '\n')
+        entrada.write("0\n")
 
-generarEntradaPesosUniformes(10, 1, 30, 0.5, "uniforme_10nodos_1a30.in")
-generarEntradaPesosNormales(10, 30, 5, 0.5, "normal_10nodos_mu30_sigma5.in")
+    return
+
+pesosUniformes = generarPesosUniformes(4)
+pesosNormales = [ np.random.randint(0,10) for i in range(24) ]
+
+generarEntradaSpidermanPesosNormales(6, 1, 10, 2, archivo="spiderman.in")
+
+# generarEntradaPesosUniformes(10, 1, 30, 0.5, "uniforme_10nodos_1a30.in")
+# generarEntradaPesosNormales(10, 30, 5, 0.5, "normal_10nodos_mu30_sigma5.in")
