2a5aba14f9912a3c69e120b1a24964bbe09540d3
Cat <catalinajuarros@gmail.com>
2017-10-07 19:36:25 -0300

    Funciona el DFS para sacar backedges

--- ej2/generador_de_tests.py
+++ ej2/generador_de_tests.py
@@ -15,7 +15,79 @@ def generarPesosUniformes(n, minimo=0, maximo=10):
 def generarPesosNormales(n, mu=5, sigma=1):
     return [ int(max(np.random.normal(mu, sigma), 0)) for i in range(n*(n-1) / 2) ]
 
-def generarGrafo(n, pesos, p_backedge=0.5):
+def sacarBackedges(n, grafo, p_backedge=0.5):
+    # Elimina cada backedge de un grafo con una probabilidad 1.0 - p_backedge.
+    vecinosYPesos = [ [] for i in range(n) ]
+
+    for e in grafo:
+        vecinosYPesos[e[0] - 1].append([ e[1], e[2] ])
+        vecinosYPesos[e[1] - 1].append([ e[0], e[2] ])
+
+    color = dict([ (i+1, "blanco") for i in range(n) ])
+    backedges = backedgesDFS(vecinosYPesos, color, 1, -1, [])
+
+    for b in backedges:
+        p = np.random.uniform()
+        if p >= p_backedge:
+            grafo.remove(b)
+
+    return
+
+def backedgesDFS(vecinosYPesos, color, v, padre, res):
+    color[v] = "gris"
+
+    for t in vecinosYPesos[v-1]:
+        w = t[0]
+        if color[w] == "blanco":
+            backedgesDFS(vecinosYPesos, color, w, v, res)
+        if color[w] == "gris" and w != v and w != padre:
+            l, r = min(v,w), max(v,w)
+            res.append([l, r, t[1]])
+
+    color[v] = "negro"
+
+    return res
+
+def generarGrafoSpiderman(n, l, pesos, p_backedge=0.5):
+    # n = número de rayos
+    # l = número de capas
+    assert(len(pesos) == 2*n*l)
+
+    grafoSpiderman = []
+    pesoActual = 0
+
+    # inicializa la primera capa
+    for j in range(2, n+2):
+        grafoSpiderman.append( [1, j, pesos[pesoActual]] )
+
+        if j < n+1:
+            grafoSpiderman.append( [j, j+1, pesos[pesoActual + 1]] )
+        else:
+            grafoSpiderman.append( [2, j, pesos[pesoActual + 1]] )
+
+        pesoActual += 2
+
+    # genera las capas 2 a l
+    for i in range(1, l):
+        for j in range(2, n+2):
+            grafoSpiderman.append( [j + (i-1)*n, j + i*n, pesos[pesoActual]] )
+
+            if j < n+1:
+                grafoSpiderman.append( [j + i*n, j + i*n + 1, pesos[pesoActual + 1]] )
+            else:
+                grafoSpiderman.append( [2 + i*n, j + i*n, pesos[pesoActual + 1]] )
+
+            pesoActual += 2
+
+    print(grafoSpiderman)
+    sacarBackedges(n*l + 1, grafoSpiderman, p_backedge)
+    print(grafoSpiderman)
+
+    grafoStr = [ " ".join( str(k) for k in e ) for e in grafoSpiderman ]
+
+    return grafoStr
+
+def generarGrafoAleatorio(n, pesos, p_backedge=0.5):
     # Es necesario que los pesos se correspondan con la cantidad de aristas
     # de un grafo completo de n nodos
     assert(len(pesos) == n*(n-1) / 2)
@@ -23,14 +95,16 @@ def generarGrafo(n, pesos, p_backedge=0.5):
     # Genera un grafo completo representado por una lista de aristas.
     # El nodo de menor índice aparece antes, pero los pares no son ordenados
     # (es decir, el grafo no es dirigido).
-    grafoCompleto = []
+    grafo = []
     pesoActual = 0
     for i in range(1, n):
         for j in range(i+1, n+1):
-            grafoCompleto.append( [i, j, pesos[pesoActual]] )
+            grafo.append( [i, j, pesos[pesoActual]] )
             pesoActual += 1
 
-    grafoStr = [ " ".join( str(k) for k in e ) for e in grafoCompleto ]
+    sacarBackedges(n, grafo, p_backedge)
+
+    grafoStr = [ " ".join( str(k) for k in e ) for e in grafo ]
 
     return grafoStr
 
@@ -63,11 +137,14 @@ def generarEntradaPesosNormales(n, mu=5, sigma=1, p_backedge=0.5, archivo="outpu
     return
 
 
+
 # pesosUniformes = generarPesosUniformes(10)
 # pesosNormales = generarPesosNormales(10)
 
 # print generarGrafo(10, pesosUniformes)
 # print generarGrafo(10, pesosNormales)
 
-generarEntradaPesosUniformes(10, 1, 30, 0.5, "uniforme_10nodos_1a30.in")
-generarEntradaPesosNormales(10, 30, 5, 0.5, "normal_10nodos_mu30_sigma5.in")
+pesosUniformes = generarPesosUniformes(5)
+pesosNormales = [ np.random.randint(0,10) for i in range(24) ]
+
+generarGrafoSpiderman(6, 2, pesosNormales, 0)
